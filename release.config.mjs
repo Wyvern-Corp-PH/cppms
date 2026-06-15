4484408e450c2fc38919b1c7bd0a37c19c707d40
Julrey Rosales <julrey.rosales@simplenight.com>
2026-06-16 03:10:57 +0800

    build: monorepo root tooling and hooks

---
+++ release.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('semantic-release').GlobalConfig} */
+export default {
+  branches: ["master", "main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
